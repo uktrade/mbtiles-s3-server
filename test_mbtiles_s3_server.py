5a01466642e789d9d3f76ba21c0186799c91193c
Michal Charemza <michal@charemza.name>
2022-07-06 18:02:20 +0100

    tests: save tiles file as part of tests

--- test_mbtiles_s3_server.py
+++ test_mbtiles_s3_server.py
@@ -1,5 +1,95 @@
+from datetime import datetime
+import hashlib
+import hmac
+import urllib
+
 from mbtiles_s3_server import server
+import httpx
 
 
 def test_server():
+    put_object_no_raise('', b'')  # Ensure bucket is created
+    with open('counties.mbtiles', 'rb') as f:
+        put_object('counties.mbtiles', f.read())
     server()
+
+
+def put_object_no_raise(key, contents, params=()):
+    url = f'http://127.0.0.1:9000/my-bucket/{key}'
+    body_hash = hashlib.sha256(contents).hexdigest()
+    parsed_url = urllib.parse.urlsplit(url)
+
+    headers = aws_sigv4_headers(
+        'AKIAIOSFODNN7EXAMPLE', 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+        (), 's3', 'us-east-1', parsed_url.netloc, 'PUT', parsed_url.path, params, body_hash,
+    )
+    httpx.put(url, params=params, data=contents, headers=dict(headers))
+
+
+def put_object(key, contents, params=()):
+    url = f'http://127.0.0.1:9000/my-bucket/{key}'
+    body_hash = hashlib.sha256(contents).hexdigest()
+    parsed_url = urllib.parse.urlsplit(url)
+
+    headers = aws_sigv4_headers(
+        'AKIAIOSFODNN7EXAMPLE', 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+        (), 's3', 'us-east-1', parsed_url.netloc, 'PUT', parsed_url.path, params, body_hash,
+    )
+    response = httpx.put(url, params=params, data=contents, headers=dict(headers))
+    response.raise_for_status()
+
+
+def aws_sigv4_headers(access_key_id, secret_access_key, pre_auth_headers,
+                      service, region, host, method, path, params, body_hash):
+    algorithm = 'AWS4-HMAC-SHA256'
+
+    now = datetime.utcnow()
+    amzdate = now.strftime('%Y%m%dT%H%M%SZ')
+    datestamp = now.strftime('%Y%m%d')
+    credential_scope = f'{datestamp}/{region}/{service}/aws4_request'
+
+    pre_auth_headers_lower = tuple((
+        (header_key.lower(), ' '.join(header_value.split()))
+        for header_key, header_value in pre_auth_headers
+    ))
+    required_headers = (
+        ('host', host),
+        ('x-amz-content-sha256', body_hash),
+        ('x-amz-date', amzdate),
+    )
+    headers = sorted(pre_auth_headers_lower + required_headers)
+    signed_headers = ';'.join(key for key, _ in headers)
+
+    def signature():
+        def canonical_request():
+            canonical_uri = urllib.parse.quote(path, safe='/~')
+            quoted_params = sorted(
+                (urllib.parse.quote(key, safe='~'), urllib.parse.quote(value, safe='~'))
+                for key, value in params
+            )
+            canonical_querystring = '&'.join(f'{key}={value}' for key, value in quoted_params)
+            canonical_headers = ''.join(f'{key}:{value}\n' for key, value in headers)
+
+            return f'{method}\n{canonical_uri}\n{canonical_querystring}\n' + \
+                   f'{canonical_headers}\n{signed_headers}\n{body_hash}'
+
+        def sign(key, msg):
+            return hmac.new(key, msg.encode('ascii'), hashlib.sha256).digest()
+
+        string_to_sign = f'{algorithm}\n{amzdate}\n{credential_scope}\n' + \
+                         hashlib.sha256(canonical_request().encode('ascii')).hexdigest()
+
+        date_key = sign(('AWS4' + secret_access_key).encode('ascii'), datestamp)
+        region_key = sign(date_key, region)
+        service_key = sign(region_key, service)
+        request_key = sign(service_key, 'aws4_request')
+        return sign(request_key, string_to_sign).hex()
+
+    return (
+        (b'authorization', (
+            f'{algorithm} Credential={access_key_id}/{credential_scope}, '
+            f'SignedHeaders={signed_headers}, Signature=' + signature()).encode('ascii')
+         ),
+        (b'x-amz-date', amzdate.encode('ascii')),
+        (b'x-amz-content-sha256', body_hash.encode('ascii')),
+    ) + pre_auth_headers
