a4fbfd4ac09d42d8ec65e4ab421aaa849de46ecc
Michal Charemza <michal@charemza.name>
2022-07-06 17:04:19 +0100

    build: initial setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='mbtiles-s3-server',
+    version='0.0.0',
+    author='Department for International Trade',
+    author_email='sre@digital.trade.gov.uk',
+    description='Server to on-the-fly extract and serve vector tiles from an mbtiles file on S3',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uktrade/mbtiles-s3-server',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.5.0',
+    install_requires=[
+        'gevent>=21.12.0',
+    ],
+    py_modules=[
+        'mbtiles_s3_server',
+    ],
+)
