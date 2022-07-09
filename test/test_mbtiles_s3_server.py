4501488576c0186ba21be2380528a785682a0199
Michal Charemza <michal@charemza.name>
2022-07-09 14:29:11 +0100

    feat: include exactly fonts we need, and simpler versioning

--- test/test_mbtiles_s3_server.py
+++ test/test_mbtiles_s3_server.py
@@ -136,8 +136,8 @@ def test_tile_file_does_not_exists(processes):
 
 def test_styles_file(processes):
     response = httpx.get(
-        'http://127.0.0.1:8080/v1/styles/positron-gl-style@1.0.0-9ff6653/'
-        'style.json?fonts=fonts-gl@1.0.0-e1c6ea6&tiles=mytiles@1.1'
+        'http://127.0.0.1:8080/v1/styles/positron-gl-style@1.0.0/'
+        'style.json?fonts=fonts-gl@1.0.0&tiles=mytiles@1.1'
     )
     assert response.status_code == 200
 
@@ -150,32 +150,32 @@ def test_styles_file(processes):
         },
     }
     assert style_dict['glyphs'] == \
-        'http://127.0.0.1:8080/v1/fonts/fonts-gl@1.0.0-e1c6ea6/{fontstack}/{range}.pbf'
+        'http://127.0.0.1:8080/v1/fonts/fonts-gl@1.0.0/{fontstack}/{range}.pbf'
 
 
 def test_styles_file_does_not_exists(processes):
     response = httpx.get(
         'http://127.0.0.1:8080/v1/styles'
-        '/positron-gl-style@1.0.0-9ff6653/notmystyle.json?tiles=mytiles@1.1')
+        '/positron-gl-style@1.0.0/notmystyle.json?tiles=mytiles@1.1')
     assert response.status_code == 404
 
 
 def test_styles_file_without_tiles_argument(processes):
     response = httpx.get(
-        'http://127.0.0.1:8080/v1/styles/positron-gl-style@1.0.0-9ff6653/style.json')
+        'http://127.0.0.1:8080/v1/styles/positron-gl-style@1.0.0/style.json')
     assert response.status_code == 400
 
 
 def test_styles_file_without_tiles_version(processes):
     response = httpx.get(
-        'http://127.0.0.1:8080/v1/styles/positron-gl-style@1.0.0-9ff6653/style.json?tiles=mytiles')
+        'http://127.0.0.1:8080/v1/styles/positron-gl-style@1.0.0/style.json?tiles=mytiles')
     assert response.status_code == 400
 
 
 def test_styles_file_with_tiles_that_does_not_exists(processes):
     response = httpx.get(
         'http://127.0.0.1:8080/v1/styles'
-        '/positron-gl-style@1.0.0-9ff6653/style.json?tiles=notmytiles@1.1')
+        '/positron-gl-style@1.0.0/style.json?tiles=notmytiles@1.1')
     assert response.status_code == 404
 
 
@@ -193,7 +193,7 @@ def test_static_file_not_exist(processes):
 
 def test_font_file(processes):
     response = httpx.get(
-        'http://127.0.0.1:8080/v1/fonts/fonts-gl@1.0.0-e1c6ea6/Metropolis Black/0-255.pbf')
+        'http://127.0.0.1:8080/v1/fonts/fonts-gl@1.0.0/Metropolis Regular/0-255.pbf')
     assert response.status_code == 200
     assert len(response.content) > 1000
 
@@ -201,7 +201,7 @@ def test_font_file(processes):
 def test_font_files(processes):
     response = httpx.get(
         'http://127.0.0.1:8080/v1/fonts'
-        '/fonts-gl@1.0.0-e1c6ea6/Metropolis Black,Noto Sans Regular/0-255.pbf')
+        '/fonts-gl@1.0.0/Metropolis Regular,Noto Sans Regular/0-255.pbf')
     assert response.status_code == 200
     assert len(response.content) > 1000
 
