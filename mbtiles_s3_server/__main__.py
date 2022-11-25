402007be45ffc600a0c6fc88015cd79ba773051a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 23:21:24 +0000

    Adding tarfile member sanitization to extractall()

--- mbtiles_s3_server/__main__.py
+++ mbtiles_s3_server/__main__.py
@@ -58,7 +58,26 @@ def mbtiles_s3_server(
         tempdir = exit_stack.enter_context(tempfile.TemporaryDirectory())
         real_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
         with tarfile.open(real_path, 'r:gz') as f:
-            f.extractall(tempdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, tempdir)
         return tempdir
 
     mbtiles_dict = {
